a711381335db6febd85421366facbfecddaeddb3
soarflat <soarflat777@gmail.com>
2018-02-22 01:56:41 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Prettier tutorial
+
+各ファイルの詳細は以下の記事をご覧ください。
+
+[Prettier 入門](https://qiita.com/soarflat/items/06377f3b96964964a65d)
+
+## 使い方
+### clone
+
+```
+git clone git@github.com:hira777/prettier-tutorial.git
+cd prettier-tutorial
+```
+
+### install
+
+```
+npm install
+```
+
+or
+
+```
+yarn
+```
+
+### `eslint --fix`の実行
+
+```
+eslint --fix app.js
+```
---
+++ app.js
@@ -0,0 +1,24 @@
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+//
+// const profile = {
+//   name: 'soarflat',
+//   sex: 'male',
+//   location: 'Tokyo',
+// };
+//
+// const hoge = () => {
+//   console.log('hoge');
+// };
+//
+// const fooBar = (foo, bar) => {
+//   console.log(foo);
+//   console.log(bar);
+// };
+//
+// fooBar(111, {
+//   hoge: 'hoge!',
+// });
