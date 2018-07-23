db635673d2d4c60fac723e9416617da505106b1e
soarflat <soarflat777@gmail.com>
2018-07-24 01:45:07 +0900

    easier to check Pre-commit Hook

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-// class Person {
-//   constructor(name) {
-//     this.name = name;
-//   }
-// }
-//
-// const profile = {
-//   name: 'soarflat',
-//   sex: 'male',
-//   location: 'Tokyo',
-// };
-//
-// const hoge = () => {
-//   console.log('hoge');
-// };
-//
-// const fooBar = (foo, bar) => {
-//   console.log(foo);
-//   console.log(bar);
-// };
-//
-// fooBar(111, {
-//   hoge: 'hoge!',
-// });
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+new Person('soarflat');
+
+const profile = {
+  name: 'soarflat',
+  sex: 'male',
+  location: 'Tokyo',
+};
+
+console.log(profile.name);
+
+// const hoge = () => console.log('hoge');
+
+const fooBar = (foo, bar) => {
+  console.log(foo);
+  console.log(bar);
+};
+
+fooBar(111, {
+  hoge: 'hoge!',
+});
